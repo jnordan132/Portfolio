90bd2e42e442c1fd70838abe05b2b8aa53ab710e
jacob nordan <jacob.nordan113322@gmail.com>
2022-02-10 19:49:38 -0800

    added a bunch of new ideas to site

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import Nav from "./components/Nav";
 import Header from './components/Header'
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
+import Map from "./components/Map";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <br />
-      <br />
-      <br />
-      <br /> */}
       <Header />
       <About />
       <Portfolio />
-      <Contact />
+      <Map />
       <Footer />
     </div>
   );
